85825e245ae606c0e898aa5e1ceabbd07c8284aa
Mert <mertoktanfb@gmail.com>
2021-03-21 00:25:25 +0300

    second commit

---
+++ Rocky/Controllers/CategoryController1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController1 : Controller
+    {
+        private readonly ApplicationDbContext db;
+        private ApplicationDbContext _db;
+
+        public CategoryController1(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+    }
+}

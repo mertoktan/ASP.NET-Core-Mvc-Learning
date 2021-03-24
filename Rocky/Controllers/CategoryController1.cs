7d39ea855288441f6b9a573d0e23705829dddae5
Mert <mertoktanfb@gmail.com>
2021-03-24 23:44:26 +0300

    update commit

--- Rocky/Controllers/CategoryController1.cs
+++ Rocky/Controllers/CategoryController1.cs
@@ -35,9 +35,74 @@ namespace Rocky.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.Category.Add(obj);
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+        //Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj==null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+        //EDİT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        //Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+        //Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if (obj==null)
+            {
+                return NotFound();
+            }
+            _db.Category.Remove(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
+
         }
     }
 }
--- Rocky/Models/Category.cs
+++ Rocky/Models/Category.cs
@@ -11,8 +11,11 @@ namespace Rocky.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Required]
+        [Range(1,int.MaxValue,ErrorMessage ="Display Order for Category must be greater than 0")]
         public int Order { get; set; }
 
     }

190cbe507b2559a3abfb54e0b6272c3c1e6cd918
Mert <mertoktanfb@gmail.com>
2021-04-05 00:13:50 +0300

    update commit

--- Rocky/Controllers/ProductController1.cs
+++ Rocky/Controllers/ProductController1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Rocky.Data;
 using Rocky.Models;
 using System;
@@ -34,6 +35,16 @@ namespace Rocky.Controllers
         //GET UPSERT
         public IActionResult Upsert(int? id)
         {
+            IEnumerable<SelectListItem> CategoryDropDown = _db.Category.Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
+
+            ViewBag.CategoryDropDown = CategoryDropDown;
+
+
+
             Product product = new Product();
             if (id==null)
             {
